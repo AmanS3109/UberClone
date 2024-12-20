2cb424f45ebecc73ef841612171896746053a4bf
Aman Singh <https://github.com/AmanS3109/Programming>
2024-12-20 15:32:07 +0530

    commited message

--- frontend/src/pages/UserSignup.jsx
+++ frontend/src/pages/UserSignup.jsx
@@ -1,6 +1,29 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
 
 const UserSignup = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [userData, setUserData] = useState({})
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        setUserData({
+            fullname:{
+                firstname: firstName,
+                lastname: lastName
+            },
+            email: email,
+            password: password
+        })
+        setEmail('')
+        setPassword('')
+        setFirstName('')
+        setLastName('')
+    }
+
   return (
     <div>
         <div className="p-7 flex flex-col justify-between h-screen">
@@ -16,13 +39,17 @@ const UserSignup = () => {
                 required
                 className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-base placeholder:text-sm" 
                 type="text" 
-                placeholder="first name" 
+                placeholder="first name"
+                 value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
                 />
                 <input
                 required
                 className="bg-[#eeeeee] w-1/2 rounded px-4 py-2 border text-base placeholder:text-sm" 
                 type="etext" 
-                placeholder="last name" 
+                placeholder="last name"
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)} 
                 />
                 </div>
 
@@ -31,7 +58,9 @@ const UserSignup = () => {
                 required
                 className="bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-base placeholder:text-sm" 
                 type="email" 
-                placeholder="email@example.com" 
+                placeholder="email@example.com"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} 
                 />
 
                 <h3 className="text-base font-medium mb-2">Enter password</h3>
@@ -39,20 +68,20 @@ const UserSignup = () => {
                 required 
                 className="bg-[#eeeeee] mb-6 rounded px-4 py-2 border w-full text-base placeholder:text-sm" 
                 type="password" 
-                placeholder="password" 
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} 
                 />
 
                 <button
                 className="bg-[#111] text-white font-semibold mb-7 rounded px-4 py-2 w-full text-base placeholder:text-sm"
-                >Login</button>
+                >SignUp</button>
                 <p className="text-center">ALready have a account?<Link to='/login' className="text-blue-600 block">Login here</Link> </p>
             </form>
         </div>
         <div>
-            <Link
-            to='/captain-login'
-            className="bg-[#10b461] flex items-center justify-center text-white font-semibold mb-7 rounded px-4 py-2 w-full text-lg placeholder:text-base"
-            >Sign in as Captain</Link>
+        <p className='text-[10px] leading-tight'>This site is protected by reCAPTCHA and the <span className='underline'>Google Privacy
+            Policy</span> and <span className='underline'>Terms of Service apply</span>.</p>
         </div>
     </div>
     </div>
